a8a3333c15579c5f15da69942952d115f5ac8ee3
Francesco Mondora <frank@Akasha.local>
2015-06-04 11:54:07 +0200

    adding other recruitment messages

--- app/models/bot/tasks/message/RecruitmentMessages.scala
+++ app/models/bot/tasks/message/RecruitmentMessages.scala
@@ -4,12 +4,17 @@ package models.bot.tasks.message
  * A list of fun, pre-defined message trees.
  */
 object RescruitmentMessages {
-  def hi=MessageTree( value = "hi!!", right= howareyou, left=None );
+
+  def hi=MessageTree( value = "hey {name}, hi!!", right= howareyou, left=sosorry );
+  def sosorry=Some(MessageTree( value="I'm so sorry about that, what happen?", right=howareyou, left=None));
   def howareyou=Some(MessageTree( value = "How are you?", right=None, left=None ));
+  def hello=MessageTree( value = "Hello {name}!", right=None, left=None );
 
 
-  def hello=MessageTree( value = "Hello!", right=None, left=None );
 
+  //def occhioAzzurro=Occhio(colore="azzrurro");
+
+  //def kirsten=Ruffoni( capelli="blue", occhi=occhioAzzurro, cavallo="Rocky");
 
 
 
