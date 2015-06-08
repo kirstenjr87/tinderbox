482aa86eb09e719ecb3957160da95b263424d57c
Kirsten Ruffoni <kirsten.ruffoni@mondora.com>
2015-06-08 11:14:13 +0200

    Update RecruitmentMessages.scala
    to be continued with more phrases

--- app/models/bot/tasks/message/RecruitmentMessages.scala
+++ app/models/bot/tasks/message/RecruitmentMessages.scala
@@ -5,11 +5,22 @@ package models.bot.tasks.message
  */
 object RescruitmentMessages {
 
+//exmple of how to do it
   def hi=MessageTree( value = "hey {name}, hi!!", right= howareyou, left=sosorry );
   def sosorry=Some(MessageTree( value="I'm so sorry about that, what happen?", right=howareyou, left=None));
   def howareyou=Some(MessageTree( value = "How are you?", right=None, left=None ));
   def hello=MessageTree( value = "Hello {name}!", right=None, left=None );
-
+  def wherefrom=MessageTree( value= "Where are you from?", right=None, left=None);
+  
+//set 1
+def hi=MessageTree( value= "Hi!", right=howareyou, left=None);
+def howareyou=MessageTree( value= "How are you?", right=imfine, left=sosorry);
+def imfine=Some(MessageTree( value= "I'm fine too thanks!") right=wherefrom, left=None);
+def sosorry=Some(MessageTree( value= "I'm sorry, how come?", right=None, left=None));
+def wherefrom=MessageTree( value= "Where are you from?", right=niceplace, left=None);
+def niceplace=Some(MessageTree( value= "Nice! I've never been there...what's it like?", right=interesting, left=None));
+def interesting=MessageTree( value= "Haha, interesting", right=None, left=None);
+def imfrom=MessageTree( value= "I'm from {location} ", right=None, left=None)
 
 
   //def occhioAzzurro=Occhio(colore="azzrurro");
